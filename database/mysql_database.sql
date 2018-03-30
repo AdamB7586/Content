cb5a5ee827c7503a23b01e003bd03ae2f8b9c20b
Adam Binnersley <abinnersley@gmail.com>
2018-03-30 16:36:02 +0100

    Development update

--- database/mysql_database.sql
+++ database/mysql_database.sql
@@ -1,1 +1,28 @@
+DROP TABLE IF EXISTS `links`;
+CREATE TABLE IF NOT EXISTS `links` (
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `site_id` int(10) UNSIGNED NOT NULL,
+  `link` varchar(255) NOT NULL,
+  `link_text` varchar(50) NOT NULL,
+  `image` varchar(155) DEFAULT NULL,
+  `image_width` int(10) UNSIGNED NOT NULL DEFAULT '0',
+  `image_height` int(10) UNSIGNED NOT NULL DEFAULT '0',
+  `active` tinyint(3) UNSIGNED NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`),
+  KEY `site_id` (`site_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+DROP TABLE IF EXISTS `pages`;
+CREATE TABLE IF NOT EXISTS `pages` (
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `site_id` int(10) UNSIGNED NOT NULL,
+  `url` varchar(150) NOT NULL,
+  `title` varchar(150) NOT NULL,
+  `content` text NOT NULL,
+  `description` varchar(200) NOT NULL,
+  `active` tinyint(3) UNSIGNED NOT NULL DEFAULT '1',
+  `required` tinyint(3) UNSIGNED NOT NULL DEFAULT '0',
+  `last_update` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `unique_page` (`site_id`,`url`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
--- src/Link.php
+++ src/Link.php
@@ -10,10 +10,10 @@ class Link {
     protected $image;
     protected $siteID;
     
-    protected $link_table = 'pages';
+    protected $link_table = 'links';
 
     /**
-     * 
+     * Constructor
      * @param Database $db
      * @param int|false $siteID
      */
@@ -26,41 +26,51 @@ class Link {
     }
     
     /**
-     * 
-     * @param type $table
+     * Sets the table name where the links will be stored
+     * @param string $table This should be the table where you want the links to be stored
      * @return $this
      */
     public function setLinkTable($table){
-        if(is_string($table)){
+        if(is_string($table) && !empty(trim($table))){
             $this->link_table = trim($table);
         }
         return $this;
     }
     
     /**
-     * 
-     * @return type
+     * Return the link table string
+     * @return string
      */
     public function getLinkTable(){
         return $this->link_table;
     }
     
     /**
+     * Set the folder where the images will be uploaded to 
+     * @param string $folder This should be the name of the folder that the main images will be uploaded to
+     * @return $this
+     */
+    public function setImageFolder($folder){
+        $this->image->setImageFolder($folder);
+        return $this;
+    }
+    
+    /**
      * 
-     * @param type $linkInfo
-     * @param type $image
-     * @return type
+     * @param array $linkInfo
+     * @param array $image
+     * @return boolean If successfully added will return true else returns false
      */
     public function addLink($linkInfo, $image = NULL) {
         return $this->db->insert($this->getLinkTable(), $linkInfo);
     }
     
     /**
-     * 
-     * @param type $linkID
-     * @param type $linkInfo
-     * @param type $image
-     * @return boolean
+     * Edit a link and its information
+     * @param int $linkID
+     * @param array $linkInfo
+     * @param array $image
+     * @return boolean If successfully updated will return true else returns false
      */
     public function editLink($linkID, $linkInfo, $image = NULL) {
         if(is_numeric($linkID) && is_array($linkInfo)){
@@ -70,9 +80,9 @@ class Link {
     }
     
     /**
-     * 
-     * @param type $linkID
-     * @return boolean
+     * Delete a link and any associated files
+     * @param int $linkID This should be the unique link ID
+     * @return boolean If successfully deleted will return true else returns false
      */
     public function deleteLink($linkID) {
         if(is_numeric($linkID)){
@@ -82,10 +92,10 @@ class Link {
     }
     
     /**
-     * 
-     * @param type $linkID
-     * @param type $status
-     * @return boolean
+     * Change the active status of a link in the database
+     * @param int $linkID This should be the unique link ID
+     * @param int $status This should be the new status to assign to the link
+     * @return boolean If successfully updated will return true else returns false
      */
     public function disableLink($linkID, $status = 0) {
         if(is_numeric($linkID) && is_numeric($status)){
